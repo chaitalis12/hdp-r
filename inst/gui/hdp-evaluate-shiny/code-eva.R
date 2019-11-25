8f79827f67a96ea4b631b7f5bc91a6a516385f2e
chaitalis12 <chaitalis12@gmail.com>
2019-11-24 21:18:56 -0800

    Upload file

---
+++ inst/gui/hdp-evaluate-shiny/code-eva.R
@@ -0,0 +1,25 @@
+  
+Statement1.1<-"HDP Model Evaluation"
+Statement2.1 <- "Instructions"
+Statement3.1 <-"Instructions"
+Statement4.1 <-"If you have been invited to this page you are an expert in your field and we
+          greatly appreciate the time you're taking to give your input - Thanks!
+          To get started click the button at the bottom and then click on the
+          Comparisons tab to complete your evaluation."
+Statement5.1 <-"This evaluation is part of the Hierarchical Decision Making (HDM) method.
+          In this method a decision has been broken down into it's elements as a tree
+          and you have been asked to compare each element in the tree against the others in
+          proportion of their relative importance to the objective. Use the sliders
+          on the Comparisons page to rate each pair. For example:"
+Statement6.1 <-"If A is 3 times as important as B, A gets 75 points, B gets 25 points"
+Statement7.1 <-"If the importance of A and B are the same, both get 50 points. This is
+             the case regardless of whether both are extremely important, mildly important
+             or unimportant."
+Statement8.1 <-"If A is ¼ as important as B, A gets 20 points, B gets 80 points."
+Statement9.1 <-"Zero is not used in the pairwise comparisons. If the importance of
+             A is negligible in comparison to B, A gets 1 point, B gets 99 points. "
+Statement10.1 <-"Ready? Load the evaluations!"
+Statement11.1<-"Comparisons"
+Statement12.1 <-"Compare each item against the other"
+Statement13.1 <-"Submit your evaluation"
+  
